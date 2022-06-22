342d1817e78fb06b264684997f9652560e7342fb
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2022-06-22 18:40:47 -0300

    Fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {
   navbar, route, start
-} from 'https://cdn.jsdelivr.net/gh/marcodpt/app@0.0.4/index.js'
+} from 'https://cdn.jsdelivr.net/gh/marcodpt/app@0.1.1/index.js'
 
 navbar({
   type: 'dark',
