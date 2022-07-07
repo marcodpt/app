72282b003cd8c9072023947123e1ed7188f28868
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2022-07-07 16:40:35 -0300

    better viewer

--- js/viewer.js
+++ js/viewer.js
@@ -1,8 +1,23 @@
-export default ({name}, {jsb, wrap, get, back}) => get('api/files?id_='+name)
+export default ({name}, {jsb, wrap, get, back}) => get('api/files?name='+name)
   .then(data => {
     if (!data[0]) {
       throw 'ERROR_NOT_FOUND'
     }
+
+    const mime = data[0].mime
+    if (
+      mime.substr(-1) == '*' &&
+      mime.indexOf('audio/') != 0 &&
+      mime.indexOf('image/') != 0 &&
+      mime.indexOf('video/') != 0
+    ) {
+      window.open(location.href.replace('#/', ''), '_blank')
+      return {
+        mime: '',
+        data: ''
+      }
+    }
+
     return get('api/files/'+data[0].id)
   }).then(({
     data,
